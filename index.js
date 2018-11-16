898bb05ef037a485af3bd227a58f19ac65652293
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-11-16 05:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ function theBeatlesPlay(musicians, instruments){
 
 /////////////////////////////////////////////////////////
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
-
 function johnLennonFacts(facts){
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ]
   const yelledFacts = []
 
   let i = 0
