072499a71caf146bfa7625eb04184ccdbd090335
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-11-16 05:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,20 +26,7 @@ function theBeatlesPlay(musicians, instruments){
   return arr
 }
 
-// SOLUTION:
-// function theBeatlesPlay(musicians, instruments) {
-//   const arr = []
-//
-//   for (let i = 0, l = musicians.length; i < l; i++) {
-//     arr.push(`${musicians[i]} plays ${instruments[i]}`)
-//   }
-//
-//   return arr
-// }
 
-
-
-/////////////////////////////////////////////////////////
 
 function johnLennonFacts(facts){
   const yelledFacts = []
@@ -53,34 +40,14 @@ function johnLennonFacts(facts){
   return yelledFacts
 }
 
-// SOLUTION:
-// function johnLennonFacts(facts) {
-//   const shoutedFacts = []
-//
-//   let i = 0
-//   while (i < facts.length) {
-//     shoutedFacts.push(`${facts[i]}!!!`)
-//     i++
-//   }
-//
-//   return shoutedFacts
-// }
-//
+function iLoveTheBeatles(n) {
+  const arr = []
 
+  do {
+    arr.push(`I love the Beatles!`)
+    n++
+  } while (n < 15)
 
-/////////////////////////////////////////////////////
+  return arr
+}
 
-function iLoveTheBeatles
-
-
-// SOLUTION:
-// function iLoveTheBeatles(n) {
-//   const arr = []
-//
-//   do {
-//     arr.push('I love the Beatles!')
-//     n++
-//   } while (n < 15)
-//
-//   return arr
-// }
